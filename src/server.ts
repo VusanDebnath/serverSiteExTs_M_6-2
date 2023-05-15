3356a460bbb9faa553be5c6869caed63a59b79d0
SM Vusan Debnath <112760155+VusanDebnath@users.noreply.github.com>
2023-05-15 17:41:11 +0600

    string patterns

--- src/server.ts
+++ src/server.ts
@@ -20,6 +20,21 @@ app.all("/allrequest", (req: Request, res:Response) => {
     res.send("All request type can be accepted!!")
 });
 
+
+// Route Paths String Patterns
+app.get("/ab*cd", (req: Request, res: Response) => {
+    res.send("String Patterns. For know more info goto express js routing string patterns!!")
+});
+
+// Router Parameters
+app.get("/params/:userName/:userId/:bookId", (req: Request, res: Response) => {
+    console.log(req.params);
+    res.json(req.params);
+});
+
+
+
+
 app.listen(port, () => {
     console.log(`Server has Started on port ${port}`);
 });

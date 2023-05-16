0e0f86cf37723ee096e514da0f7811ecf712c66b
SM Vusan Debnath <112760155+VusanDebnath@users.noreply.github.com>
2023-05-16 22:38:47 +0600

    add find and findOne

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,12 +1,31 @@
 import { Request, Response } from "express"
-import { createUserToDB } from "./user.service";
+import { createUserToDB, getUserByIdFromDB, getUsersFromDB } from "./user.service";
 
 
 export const createUser =async (req: Request, res: Response) => {
-    const user = await createUserToDB();
+
+    const data = req.body;
+    const user = await createUserToDB(data);
 
     res.status(200).json({
         status: 'success',
+        data: user,
+    });
+};
+
+export const getUsers = async (req: Request, res: Response) => {
+    const user = await getUsersFromDB();
+    res.status(200).json({
+        status: "success",
+        data: user,
+    })
+};
+
+export const getUserById = async(req: Request, res: Response) => {
+    const { id } = req.params;
+    const user = await getUserByIdFromDB(id);
+    res.status(200).json({
+        status: "Success",
         data: user,
     });
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,12 +1,40 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
 
 
- // make a async function
+// make a async function
 
- //normal export korbo karon aro onno query hote pare
+//normal export korbo karon aro onno query hote pare
 
- export const createUserToDB = async () => {
-    //4. create a instance
+export const createUserToDB = async (payload: IUser): Promise<IUser> => {
+    //create a new user
+    const user = new User(payload); // User -> class user-> instance
+    await user.save();
+    return user;
+};
+
+
+export const getUsersFromDB = async (): Promise<IUser[]> => {
+    const users = await User.find();
+    return users;
+};
+
+
+export const getUserByIdFromDB = async (payload: string): Promise<IUser | null> => {
+
+    const user = await User.findOne({ id: payload });
+    // const user = await User.findOne({ id: payload },{name:1,email:1, id:1, _id: 0});
+
+    //Field Filtering 1 value is true and 0 value for false {which i don't want}
+    return user;
+};
+
+
+
+
+
+/* 
+4. create a instance
     const user = new User({
         id: 102,
         role: "student",
@@ -17,6 +45,5 @@ import User from "./user.model";
         gender: "male",
         email: "abc@gmail.com"
     });
-    await user.save();
-    return user;
-};
+
+*/
